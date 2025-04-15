ac1749c3ee120d9f0d8addcf53911329fa256d9f
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-04-15 21:48:26 +0800

    Update login.php

--- web/Devonpart/web/login.php
+++ web/Devonpart/web/login.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $stmt->execute();
             }
 
-            header("Location: dashboard.php");
+            header("Location: ../../LeonPart/Code/index.php ");
             exit();
         } else {
             echo "<script>alert('Incorrect password');</script>";
