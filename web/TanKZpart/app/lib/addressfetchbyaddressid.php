bf69c66b6a6d4135273576fc453f9989ca6e7ddc
TanKZ67 <tankz-wm24@student.tarc.edu.my>
2025-04-18 23:46:45 +0800

    a

--- web/TanKZpart/app/lib/addressfetchbyaddressid.php
+++ web/TanKZpart/app/lib/addressfetchbyaddressid.php
@@ -5,16 +5,16 @@ if ($conn->connect_error) {
     die("连接失败：" . $conn->connect_error);
 }
 
-if (isset($_GET['error']) && $_GET['error'] === 'address_name') {
-    echo "<script>document.getElementById('address_name').style.display = 'block';</script>";
-}elseif (isset($_GET['error']) && $_GET['error'] === 'floor_unit') {
-    echo "<script>document.getElementById('floor_unit').style.display = 'block';</script>";
-}elseif (isset($_GET['error']) && $_GET['error'] === 'state') {
-    echo "<script>document.getElementById('state').style.display = 'block';</script>";
-}elseif (isset($_GET['error']) && $_GET['error'] === 'district') {
-    echo "<script>document.getElementById('district').style.display = 'block';</script>";
-}elseif (isset($_GET['error']) && $_GET['error'] === 'postcode') {
-    echo "<script>document.getElementById('postcode').style.display = 'block';</script>";
+if (isset($_GET['error']) && $_GET['error'] === 'name_address') {
+    echo "<script>document.getElementById('name_address').style.display = 'block';</script>";
+}elseif (isset($_GET['error']) && $_GET['error'] === 'floor_unit2') {
+    echo "<script>document.getElementById('floor_unit2').style.display = 'block';</script>";
+}elseif (isset($_GET['error']) && $_GET['error'] === 'state2') {
+    echo "<script>document.getElementById('state2').style.display = 'block';</script>";
+}elseif (isset($_GET['error']) && $_GET['error'] === 'district2') {
+    echo "<script>document.getElementById('district2').style.display = 'block';</script>";
+}elseif (isset($_GET['error']) && $_GET['error'] === 'postcode2') {
+    echo "<script>document.getElementById('postcode2').style.display = 'block';</script>";
 }
 // 确保 $_GET['edit'] 存在，并且是一个数字
 if (isset($_GET['edit']) && is_numeric($_GET['edit'])) {
@@ -153,16 +153,16 @@ include 'addressfetch.php';
                                 <p class="name_address" id="name_address">address name is empty</p>
                             </div>
                             <div >
-                                <p class="floor_unit" id="floor_unit">floor_unit is empty</p>
+                                <p class="floor_unit" id="floor_unit2">floor_unit is empty</p>
                             </div>
                             <div >
-                                <p class="state" id="state">state is empty</p>
+                                <p class="state" id="state2">state is empty</p>
                             </div>
                             <div >
-                                <p class="district" id="district">district is empty</p>
+                                <p class="district" id="district2">district is empty</p>
                             </div>
                             <div >
-                                <p class="postcode" id="postcode">postcode is empty</p>
+                                <p class="postcode" id="postcode2">postcode is empty</p>
                             </div>
                         </div>
 
--- web/TanKZpart/app/page/update_address_by_addID.php
+++ web/TanKZpart/app/page/update_address_by_addID.php
@@ -13,23 +13,22 @@ $conn = new mysqli("localhost", "root", "", "online_shopping");
 
 
 if ($_POST["address_name"] == "") {
-    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=address_name");
+    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=name_address");
     exit;
 } elseif ($_POST["floor_unit"] == "") {
-    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=floor_unit");
+    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=floor_unit2");
     exit;
 } elseif ($_POST["state"] == "") {
-    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=state");
+    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=state2");
     exit;
 } elseif ($_POST["district"] == "") {
-    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=district");
+    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=district2");
     exit;
 } elseif ($_POST["postcode"] == "") {
-    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=postcode");
+    header("Location: ../lib/addressfetchbyaddressid.php?edit=$address_id&error=postcode2");
     exit;
 } else {
     $stmt = $conn->prepare("UPDATE user_address set address_name=?,floor_unit=?,state=?,district=?,postcode=? WHERE address_id=?");
-    echo $address_id, $address_name, $floor_unit, $state, $postcode, $district;
     $stmt->bind_param("ssssii", $address_name, $floor_unit, $state, $district, $postcode, $address_id);
 
     $stmt->execute();
