30497964931d9eeefdce35cf8b9225ae95699672
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-04-22 14:07:43 +0800

    a

--- web/ZheYongPart/admin.php
+++ web/ZheYongPart/admin.php
@@ -166,8 +166,10 @@ if (isset($_POST['add_product'])) {
 
    ?>
 
-   <a href="#" onclick="location.href='\a\Website_assignment\web\YuchenPart\Aftersignin.php'"  style="display: block; width: 10%; cursor: pointer; border-radius: .5rem; margin-top: 1rem; font-size: 1.7rem; padding: 1rem 3rem; background: var(--green);
-   color: var(--white); text-align:center;">Back</a>
+<a href="/Website_assignment/web/YuchenPart/Aftersignin.php"
+   style="display: block; width: 10%; cursor: pointer; border-radius: .5rem; margin-top: 1rem; font-size: 1.7rem; padding: 1rem 3rem; background: var(--green); color: var(--white); text-align:center;">
+   Back
+</a>
 
 
 
