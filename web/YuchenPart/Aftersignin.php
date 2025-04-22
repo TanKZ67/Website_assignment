5c92be0d60fa4ee534c239a94aa9c266f941c07a
Yuchen-TNG <tngyc-wm24@student.tarc.edu.my>
2025-04-22 11:21:03 +0800

    a

--- web/YuchenPart/Aftersignin.php
+++ web/YuchenPart/Aftersignin.php
@@ -4,36 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Aftersigin.css">
-    <title>Square Example</title>
+    <title>Dashboard</title>
 </head>
 <body>
 
-
     <div class="container">
-        <div class="left-section">
-            <h1>Memberlist</h1>
-            <div class="SPACE01">
-                <a href="Content.php">
-                    <img src="user_icon_007.jpg" class="Clickanimation" alt="User Icon">
-                </a>
+
+        <!-- 上排：Memberlist + Order History -->
+        <div class="top-row">
+            <div class="box-section">
+                <h1>Memberlist</h1>
+                <div class="SPACE01">
+                    <a href="Content.php">
+                        <img src="user_icon_007.jpg" class="Clickanimation" alt="User Icon">
+                    </a>
+                </div>
+            </div>
+
+            <div class="box-section">
+                <h1>Order History</h1>
+                <div class="SPACE02">
+                    <a href="admin_order_history.php">
+                        <img src="history_icon.png" class="Clickanimation" alt="Order History Icon">
+                    </a>
+                </div>
             </div>
         </div>
 
-        <div class="right-section">
-            <h1>Product</h1>
-            <div class="SPACE02">
-                <a href="Content.php">
-                    <img src="Product_sample_icon_picture.png" class="Clickanimation" alt="Product Image">
-                </a>
+        <!-- 中排：Product -->
+        <div class="bottom-row">
+            <div class="box-section">
+                <h1>Product</h1>
+                <div class="SPACE03">
+                    <a href="Content.php">
+                        <img src="Product_sample_icon_picture.png" class="Clickanimation" alt="Product Image">
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-    <a href="logout.php" style="color: red;">Logout</a>
 
-    <div class="button-container">
-            <a href="order_history.php" class="order-history-button">Order History</a>
-            <a href="logout.php" class="logout-button">Logout</a>
-        </div>
+    <!-- 底部 Logout 按钮 -->
+    <div class="bottom-center">
+        <a href="logout.php" class="logout-button">Logout</a>
     </div>
 
 </body>
