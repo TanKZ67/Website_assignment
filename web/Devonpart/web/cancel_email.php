35d97c2a63f49272dca07ef3d2bad1be24eab9a2
Devon-888 <devoneyc-wm24@student.tarc.edu.my>
2025-04-23 21:06:29 +0800

    a

--- web/Devonpart/web/cancel_email.php
+++ web/Devonpart/web/cancel_email.php
@@ -11,7 +11,7 @@ if (isset($_GET['email'])) {
     if ($stmt->execute()) {
         echo "<h1>Registration Canceled!</h1>";
         echo "<p>Your registration request has been canceled. If this was a mistake, please sign up again.</p>";
-        echo '<p><a href="index.php">Go back to the registration page</a></p>';
+        echo '<p><a href="register.php">Go back to the registration page</a></p>';
     } else {
         echo "<h1>Error!</h1>";
         echo "<p>There was an issue canceling your registration. Please try again later.</p>";
@@ -19,6 +19,6 @@ if (isset($_GET['email'])) {
 } else {
     echo "<h1>Invalid Request!</h1>";
     echo "<p>No valid email provided. Please make sure the link is correct.</p>";
-    echo '<p><a href="index.php">Go back to the registration page</a></p>';
+    echo '<p><a href="register.php">Go back to the registration page</a></p>';
 }
 ?>
